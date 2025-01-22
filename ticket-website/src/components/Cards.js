a3cef104b6bd6b5926aad95f4d7ba94dd5a31b92
KGesino <kgesino@gmail.com>
2025-01-22 12:45:44 -0700

    More Photos

--- ticket-website/src/components/Cards.js
+++ ticket-website/src/components/Cards.js
@@ -5,41 +5,41 @@ import './Cards.css';
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check out the ballpark!</h1>
+      <h1>Check out Arizona sports!</h1>
       <div class="cards__container">
         <div class="cards__wrapper">
           <ul class="cards__items">
             <CardItem 
             src="images/phoenix_suns_upper.jpg"
-            text="Head to Footprint Center to Catch a Suns game"
+            text="Feel the heat, catch the Suns live!"
             label='Basketball'
             path='/tickets'
             />
             <CardItem 
-            src="images/img-2.jpg"
-            text="Travel through the Islands of Bali in a Private Cruise"
-            label='Luxury'
-            path='/explore'
+            src="images/chasefield-2021-4.jpg"
+            text="Step up to the plate!"
+            label='Baseball'
+            path='/tickets'
             />
           </ul>
           <ul class="cards__items">
             <CardItem 
-            src="images/img-3.jpg"
-            text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
-            label='Mystery'
-            path='/explore'
-            />
-            <CardItem 
-            src="images/img-4.jpg"
-            text="Experience Football on Top of the Himilayan Mountains"
-            label='Adventure'
+            src="images/statefarm-stadium.jpg"
+            text="Nothing beats Game day."
+            label='Football'
             path='/tickets'
             />
             <CardItem 
-            src="images/img-8.jpg"
-            text="Ride through the Sahara Desert on a guided Camel Tour"
-            label='Adrenaline'
-            path='/sign-up'
+            src="images/Arizona-Rattlers.jpg"
+            text="Experience Arena Football at its best!"
+            label='Arena Football'
+            path='/tickets'
+            />
+            <CardItem 
+            src="images/booker-shot.jpg"
+            text="Book your tickets today!"
+            label='Basketball'
+            path='/tickets'
             />
           </ul>
         </div>
